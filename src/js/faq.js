194ee622f95f94ee412c16e74aaedaedf0382b6d
Vlad_Deineko <vladd1904@gmail.com>
2025-04-28 22:22:16 +0300

    another update

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,24 +1,14 @@
-document.querySelectorAll('.accordion-trigger').forEach(trigger => {
-  trigger.addEventListener('click', function () {
-    const item = this.parentElement;
-    const content = item.querySelector('.accordion-content');
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-    if (item.classList.contains('open')) {
-      item.classList.remove('open');
-      this.classList.remove('active');
-      content.style.maxHeight = null;
-      content.classList.remove('open');
-    } else {
-      document.querySelectorAll('.accordion-item').forEach(i => {
-        i.classList.remove('open');
-        i.querySelector('.accordion-trigger').classList.remove('active');
-        i.querySelector('.accordion-content').style.maxHeight = null;
-        i.querySelector('.accordion-content').classList.remove('open');
-      });
-      item.classList.add('open');
-      this.classList.add('active');
-      content.classList.add('open');
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+ document.addEventListener('DOMContentLoaded', function() {
+    new Accordion('#accordion', {
+      duration: 300, // скорость анимации (в миллисекундах)
+      showMultiple: false, // можно ли открывать сразу несколько блоков
+      openOnInit: [0], // можно указать, какой аккордеон открыть при загрузке (например, 0 - первый)
+      elementClass: 'ac-item', // класс элемента
+      triggerClass: 'ac-trigger', // класс триггера
+      panelClass: 'ac-panel', // класс панели
+      activeClass: 'is-active' // класс для открытого элемента
+    });
   });
-});
